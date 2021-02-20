7ec8e69bd2614485cc05de0b818ffafd0c326dfd
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-02-20 12:15:27 +0530

    added new line

--- lib/UI/book_a_trip.dart
+++ lib/UI/book_a_trip.dart
@@ -259,6 +259,7 @@ class _BookATripState extends State<BookATrip> {
                   // Expanded(flex: 1,child:Text("OPEN"),),
                   Expanded(
                     child: Container(
+                      /* Hello world*/
                       alignment: Alignment.center,
                       child: Text("OPEN"),
                       color: Colors.white,
